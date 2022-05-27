89fa54ffa19d08b8c193a9822e9de2cf324e9248
be.sotnikova <bsotnikova@mail.ru>
2022-05-27 19:48:11 +0400

    test

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,7 @@
 public class BonusMilesService {
-    public int calculate (int cost){
-        return cost;
+    public int calculate(int cost){
+        int check = 50000;
+        int miles = check /cost;
+        return miles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static <String> void main(String[] args) {
+    public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
